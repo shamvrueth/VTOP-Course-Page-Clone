c591cbff4e58b3123d3eeed99e7769f1f2ba3186
shamvrueth <145780447+shamvrueth@users.noreply.github.com>
2024-04-29 01:40:19 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -21,3 +21,23 @@ insert into subject(subject_name, semester_name) values('Structured and object o
 insert into subject(subject_name, semester_name) values('Engineering physics','Winter semester 2023-24');
 insert into subject(subject_name, semester_name) values('Differential equation','Winter semester 2023-24');
 
+create table faculty(
+	id serial primary key,
+	subject_name varchar(100),
+	slot varchar(15),
+	faculty varchar(50)
+);
+
+insert into faculty(subject_name, slot, faculty) values('Structured and object oriented programming','C2','Sahil');
+insert into faculty(subject_name, slot, faculty) values('Structured and object oriented programming','C1','Vedanth');
+
+create table download(
+	id serial primary key,
+	lecture_topic text,
+	reference text
+);
+
+ALTER TABLE download
+ADD COLUMN faculty_name varchar(50);
+
+insert into download(lecture_topic, reference, faculty_name) values('C programming','https://www.lpude.in/SLMs/Master%20of%20Computer%20Applications/Sem_1/DECAP010_PROGRAMMING_IN_C.pdf','Vedanth Prabhu');
